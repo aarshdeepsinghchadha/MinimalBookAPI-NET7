4ebaccff006b2bf59864779838285ee1d2df7e82
Aarshdeep Chadha <ascnyc29@gmail.com>
2023-06-16 16:42:32 +0530

    minimal api with entity framework done all cRUD api

---
+++ MinimalBookAPI/DataContext.cs
@@ -0,0 +1,19 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace MinimalBookAPI
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=ASCNYC;database=MinimalAPI;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Book> Books => Set<Book>();
+
+    }
+}
--- MinimalBookAPI/Program.cs
+++ MinimalBookAPI/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using MinimalBookAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
@@ -17,6 +21,53 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
+app.MapGet("/book", async (DataContext context) =>
+         await context.Books.ToListAsync());
+
+
+app.MapGet("/book/{id}", async (DataContext context,int id) => 
+    await context.Books.FindAsync(id) is Book book ? 
+    Results.Ok(book) : 
+    Results.NotFound("Book not found"));
+
+
+app.MapPost("/book", async (Book addbook, DataContext context) =>
+{
+    context.Books.Add(addbook);
+    await context.SaveChangesAsync();
+    return Results.Ok(await context.Books.ToListAsync());
+});
+
+app.MapPut("/book/{id}", async (Book updatedBook, DataContext context, int id) =>
+{
+    var book = await context.Books.FindAsync(id);
+    if (book is null)
+    { return Results.NotFound("Book not found"); }
+    book.Title = updatedBook.Title;
+    book.Author = updatedBook.Author;
+
+    context.Books.Update(book);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await context.Books.ToListAsync());
+});
+app.MapDelete("/book/{id}", async (DataContext context,int id) =>
+{
+    var book = await context.Books.FindAsync(id);
+    if (book is null)
+    { return Results.NotFound("Book not found"); }
+    context.Books.Remove(book);
+    await context.SaveChangesAsync();
+    return Results.Ok(await context.Books.ToListAsync());
+});
+
 app.Run();
+
+public class Book
+{
+    public  int Id { get; set; }
+    public required string  Title { get; set; }
+    public required string  Author { get; set; }
+}
 
 
